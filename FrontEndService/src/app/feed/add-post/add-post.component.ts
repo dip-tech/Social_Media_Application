569a91cc5f9db940e84fbaa5277dfbefdfa2590a
Dipankar Dutta <dipankar8dutta@gmail.com>
2023-10-25 20:50:23 +0530

    25-10-2023 20:50

--- FrontEndService/src/app/feed/add-post/add-post.component.ts
+++ FrontEndService/src/app/feed/add-post/add-post.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
   styleUrls: ['./add-post.component.css']
 })
-export class AddPostComponent {
+export class AddPostComponent implements OnInit {
+  $IMAGE_PATH: any = null;
+  $SHOW_IMAGE: boolean = false;
+  
 
+
+  ngOnInit(): void {
+
+  }
+
+  onFileChange(event) {
+    console.log(event.target.files[0])
+    var reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = (event) => {
+      this.$IMAGE_PATH = (<FileReader>event.target).result;
+      this.$SHOW_IMAGE = true;
+      console.log(this.$IMAGE_PATH)
+
+    }
+    
+
+  }
+
+  removeFile(){
+    this.$IMAGE_PATH=null;
+    this.$SHOW_IMAGE=false;
+  }
 }
